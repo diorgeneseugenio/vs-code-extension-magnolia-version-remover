eb80a964fd67f92de7a7c4a62efda87657e010b3
Diórgenes Eugênio da Silveira <diorgeneseugenio@gmail.com>
2023-02-06 18:16:39 -0300

    feat: add new regex to cover all cases

--- src/extension.ts
+++ src/extension.ts
@@ -15,15 +15,15 @@ export function activate(context: vscode.ExtensionContext) {
       const fileContent = document.getText();
 
       const newFileContent = fileContent
-        .replace(/  ["|']jcr:baseVersion["|']:.*\n/gi, "")
-        .replace(/  ["|']jcr:isCheckedOut["|']:.*\n/gi, "")
-        .replace(/  ["|']jcr:versionHistory["|']:.*\n/gi, "")
-        .replace(/  ["|']jcr:predecessors["|']:.*\n/gi, "")
-        .replace(/  ["|']jcr:mixinTypes["|']:.*\n/gi, "")
-        .replace(/  ["|']mgnl:lastActivated["|']:.*\n/gi, "")
-        .replace(/  ["|']mgnl:lastActivatedBy["|']:.*\n/gi, "")
-        .replace(/  ["|']mgnl:lastActivatedVersion["|']:.*\n/gi, "")
-        .replace(/  ["|']mgnl:lastActivatedVersionCreated["|']:.*\n/gi, "");
+        .replace(/.*["|']jcr:baseVersion["|']:.*\n/gi, "")
+        .replace(/.*["|']jcr:isCheckedOut["|']:.*\n/gi, "")
+        .replace(/.*["|']jcr:versionHistory["|']:.*\n/gi, "")
+        .replace(/.*["|']jcr:predecessors["|']:.*\n/gi, "")
+        .replace(/.*["|']jcr:mixinTypes["|']:.*\n/gi, "")
+        .replace(/.*["|']mgnl:lastActivated["|']:.*\n/gi, "")
+        .replace(/.*["|']mgnl:lastActivatedBy["|']:.*\n/gi, "")
+        .replace(/.*["|']mgnl:lastActivatedVersion["|']:.*\n/gi, "")
+        .replace(/.*["|']mgnl:lastActivatedVersionCreated["|']:.*\n/gi, "");
 
       editor.edit((editBuilder) => {
         editBuilder.replace(
